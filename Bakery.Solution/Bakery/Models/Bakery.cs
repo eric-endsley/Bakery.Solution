d56599bb147ff635c92c70805969f54f46e371e8
Eric Endsley <eric.endsley4@gmail.com>
2020-12-11 15:01:33 -0800

    write autoimplemented Price property with get method

--- Bakery.Solution/Bakery/Models/Bakery.cs
+++ Bakery.Solution/Bakery/Models/Bakery.cs
@@ -7,10 +7,11 @@ namespace Bakery
   {
 
     public static int Amount { get; set; }
-    
+    public static int Price { get; }
   }
   public class Pastry
   {
     public static int Amount { get; set; }
+    public static int Price { get; }
   }
 }
