1f710c31f8729cfa0d09f5841642f197a153a798
Eric Endsley <eric.endsley4@gmail.com>
2020-12-11 15:25:20 -0800

    add private sets to Price properties so methods can modify them

--- Bakery.Solution/Bakery/Models/Bakery.cs
+++ Bakery.Solution/Bakery/Models/Bakery.cs
@@ -7,12 +7,16 @@ namespace Bakery
   {
     
     public static int Amount { get; set; }
-    public static int Price { get; }
-   
+    public static int Price { get; private set; }
+
+    public static void PriceCalc(int breadAmount)
+    {
+      Bread.Price = Bread.Amount * 5;
+    }
   }
   public class Pastry
   {
     public static int Amount { get; set; }
-    public static int Price { get; }
+    public static int Price { get; private set; }
   }
 }
