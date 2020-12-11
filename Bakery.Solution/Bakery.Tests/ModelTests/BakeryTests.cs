01ae1a9c58d7150e1af6f293939e840d5273f9c0
Eric Endsley <eric.endsley4@gmail.com>
2020-12-11 10:40:37 -0800

    write test for each constructor, bread & pastry

--- Bakery.Solution/Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Solution/Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,15 +1,24 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
 
 namespace Bakery.Tests
 {
   public class BakeryTests
   {
     [TestMethod]
-    
+    public void ConstructBreadOrder_CreatesInstanceOfBread_Bread() 
     {
+      Bread breadOrder = new Bread();
+      Assert.AreEqual(typeof(Bread), breadOrder.GetType());
 
+    }
+    [TestMethod]
+    public void ConstructPastryOrder_CreatesInstanceOfPastry_Pastry() 
+    {
+      Pastry pastryOrder = new Pastry();
+      Assert.AreEqual(typeof(Pastry), pastryOrder.GetType());
     }
   }
 }
