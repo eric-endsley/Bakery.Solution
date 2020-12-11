9347ccc36c39d29a0df4bb97d1482f73cba7f851
Eric Endsley <eric.endsley4@gmail.com>
2020-12-11 14:41:55 -0800

    pass first test

--- Bakery.Solution/Bakery/Models/Bakery.cs
+++ Bakery.Solution/Bakery/Models/Bakery.cs
@@ -6,7 +6,7 @@ namespace Bakery
   public class Bread 
   {
 
-    public static int Amount { get { return 900; } set { } }
+    public static int Amount { get; set; }
     
   }
   public class Pastry
--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -6,7 +6,7 @@ namespace Bakery
   {
     public static void Main()
     {
-      
+
     }
   }
 }
