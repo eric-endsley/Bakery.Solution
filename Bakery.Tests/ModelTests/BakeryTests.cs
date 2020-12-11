d4589ef06dd4ec0ba2bbe6283586cc56ae6fe775
Eric Endsley <eric.endsley4@gmail.com>
2020-12-11 10:15:45 -0800

    add namespaces & initial classes to all cs files

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,3 +1,14 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bakery.Tests
+{
+  public class BakeryTests
+  {
+    [TestMethod]
+    {
+      
+    }
+  }
+}
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,1 +1,12 @@
 using System;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      
+    }
+  }
+}
